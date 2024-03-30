9aada57b83870386156a1b01ab98d9b57d087f92
catalinsamson01 <126816288+catalinsamson01@users.noreply.github.com>
2024-03-30 23:57:14 +0200

    Add files via upload

---
+++ pfm_report/src/main/java/com/example/pfm_report/PfmReportApplication.java
@@ -0,0 +1,13 @@
+package com.example.pfm_report;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PfmReportApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PfmReportApplication.class, args);
+	}
+
+}
---
+++ pfm_report/src/test/java/com/example/pfm_report/PfmReportApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.pfm_report;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PfmReportApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
